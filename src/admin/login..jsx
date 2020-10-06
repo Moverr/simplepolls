2774a1a33ea0484f98c12857f652980bae09b945
Muyinda ROgers <moverr@gmail.com>
2020-10-06 14:32:14 +0300

    Ultra Ligners

--- src/admin/login..jsx
+++ src/admin/login..jsx
@@ -1,14 +1,33 @@
 
   import React, { Component } from 'react';
   import PropTypes from 'prop-types';
+import InputField from '../forms/inputs/InputField';
+  
   
   class login extends Component {
       render() {
           return (
-              <div>
+              
+              <div className={'row'}>
+                    <div className={'col-md-10'}>
+                    <form role="form">
                  <h1>LOGIN FORM </h1>
+
+                 {/* username  */}
+                 <div className="form-group">
                  <label>Username : </label>
-                 
+                 <InputField className="form-control" />
+                 </div>
+
+                 {/* Password  */}
+                 <div className="form-group">
+                 <label>Password : </label>
+                 <InputField className="form-control"  type="password"/>
+                
+                 </div>
+                 </form>
+                 </div>
+               
               </div>
           );
       }
