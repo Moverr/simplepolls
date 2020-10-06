d9e344cce902b291a91cfceb8c9d40f28e178ef7
Muyinda ROgers <moverr@gmail.com>
2020-10-06 15:20:07 +0300

    :Debuggging

--- src/admin/login..jsx
+++ src/admin/login..jsx
@@ -1,40 +1,64 @@
 
-  import React, { Component } from 'react';
-  import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import InputField from '../forms/inputs/InputField';
-  
-  
-  class login extends Component {
-      render() {
-          return (
-              
-              <div className={'row'}>
-                    <div className={'col-md-10'}>
+
+
+class login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+            verifyBtn: 'VERIFY SCHOOL',
+            schoolName: '',
+            message_status: '-success displaynone',
+            message: ' Wait a minute '
+        };
+
+    }
+
+
+    handleChange(e) {
+        this.setState({ value: e.target.value });
+    }
+
+
+
+
+    render() {
+        return (
+
+            <div className={'row'}>
+                <div className={'col-md-10'}>
                     <form role="form">
-                 <h1>LOGIN FORM </h1>
+                        <h1>LOGIN FORM </h1>
 
-                 {/* username  */}
-                 <div className="form-group">
-                 <label>Username : </label>
-                 <InputField className="form-control" />
-                 </div>
+                        {/* username  */}
+                        <div className="form-group">
+                            <label>Username : </label>
+                            <InputField className="form-control"
+                                value={this.state.schoolName}
 
-                 {/* Password  */}
-                 <div className="form-group">
-                 <label>Password : </label>
-                 <InputField className="form-control"  type="password"/>
-                
-                 </div>
-                 </form>
-                 </div>
-               
-              </div>
-          );
-      }
-  }
-  
-  login.propTypes = {
-  
-  };
-  
-  export default login;
+
+                            />
+                        </div>
+
+                        {/* Password  */}
+                        <div className="form-group">
+                            <label>Password : </label>
+                            <InputField className="form-control" type="password" />
+
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+login.propTypes = {
+
+};
+
+export default login;
