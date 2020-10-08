b22192bca1868c47bdc842317e27d6e47192b289
Muyinda ROgers <moverr@gmail.com>
2020-10-08 20:49:56 +0300

    :Debugging

--- src/admin/candidate.jsx
+++ src/admin/candidate.jsx
@@ -1,15 +1,21 @@
-import React  from 'react';
+import React,{Component}  from 'react';
 import { Button } from 'react-bootstrap';
 
-const Candidate = function(props) {
-    return <div className="candidate">
-       <img src={props.img} />
-       <br/>
-       Votes :  0 <br/>
-       Percentage :  20 <br/>
-      <button className={"btn btn-primary"} >YA</button>
-      <button className={"btn btn-primary"} onClick={()=>alert('Listen movers')}>NO</button>
-    </div>;
-}
+ 
 
+function Candidate(props){
+    const { votes, profile, img, callback,id } = props;
+    return <div key={id} className="candidate">
+        <img src={img} />
+        <br/>
+        Votes :  {votes+1} <br/>
+        Percentage :  20 <br/>
+       
+       <button className={"btn btn-primary"} onClick={callback}  >YA</button>
+       <button className={"btn btn-primary"} onClick={()=>alert('Listen movers')}>NO</button>
+     </div>;
+}
 export default Candidate;
+
+
+ 
--- src/admin/polls.jsx
+++ src/admin/polls.jsx
@@ -1,64 +1,121 @@
 import React, { Component } from 'react';
-import '../css/polling.css'; 
+import '../css/polling.css';
 import Candidate from './Candidate';
- 
- 
+
+
 
 class polls extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-			polls: null			 
+            electioncandidates: null,
+            counter: 1
         };
 
         this.loadCandidate = this.loadCandidate.bind(this);
-        this.handleCallback = this.handleCallback.bind(this);
-        
-    }
 
-    componentDidMount(){
-         //fetch from server 
-         fetch('https://api.newvisionapp.com/v1/ElectoralCandidates?limit=1000')
-        .then(response => response.json())
-        .then(data => {
-            this.setState({polls:data});
-            console.log(data);
-        });
-        
-
+        this.cleanList = this.cleanList.bind(this);
 
     }
-    
+
+    componentDidMount() {
+        //fetch from server 
+        fetch('https://api.newvisionapp.com/v1/ElectoralCandidates?limit=1000')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ electioncandidates: data });
+
+            });
+
+
+
+    }
+
     render() {
+        let polldata = this.state.electioncandidates;
         return (
             <div className="wrapper">
-            {this.loadPolls()}
-           
-            
+                {this.loadPolls(polldata)}
+               
+
+
             </div>
         );
     }
 
-   
-    loadPolls() {
-        let polldata = this.state.polls;
-        let polls = Array();
-        if(polldata != null){
-            polls = polldata.map(n=>this.loadCandidate(n));
-        } 
-        return  polls;
+
+    loadPolls(polldata) {
+
+
+        let pollas = Array();
+        if (polldata != null) {
+             polldata = polldata.map(n => this.cleanList(n));
+            
+            polldata.sort((a,b)=>b.votes - a.votes)             
+            console.log(polldata);
+            pollas = polldata.map(n => this.loadCandidate(n));
+
+        }
+
+        // this.setState({polls:pollas})
+        return pollas;
+    }
+
+    cleanList(candidateprofile) {
+        
+        if (candidateprofile.votes == undefined) {
+            candidateprofile.votes = 0;
+            
+        }
+        return candidateprofile;
     }
 
     loadCandidate(candidateprofile) {
-       // return loadCandidate(n);
-       console.log(candidateprofile.id)
-       return <Candidate  img={candidateprofile.featured_image}  id={candidateprofile.id}  callback={this.handleCallback}/>
+        //console.log(candidateprofile) 
+        
+        let res = <div key={candidateprofile.id} className="candidate">
+            <img src={candidateprofile.featured_image} />
+            <br />
+        Votes :  {candidateprofile.votes} <br />
+        Percentage :  20 <br />
+
+            <button className={"btn btn-primary"} onClick={() => this.handleCallback(candidateprofile,"+")}  >YA</button>
+            <button className={"btn btn-primary"}   onClick={() => this.handleCallback(candidateprofile,"-")} >NO</button>
+        </div>
+
+
+
+        return res;
     }
-    handleCallback(candidateprofile,vote,votestatus){
-        alert("pass")
+    handleCallback(candidateprofile,status) {
+        
+        let polldata = this.state.polls;
+        this.loadPolls(polldata)
+        let cn = this.state.counter;
+        let l = cn + 1;
+        this.setState({counter:l })
+
+        let y = candidateprofile;
+        let x = (y.votes == undefined) ? 0 : y.votes;
+        let z  = x;
+        if(status == "+"){
+              z = z + 1;
+        }else{
+            if(z > 0 ){
+                z  = z - 1;
+            }
+        }
+       
+
+        candidateprofile.votes = z;
+        //this.state.counter;
+        this.loadPolls(polldata)
+
     }
-   
+
+
+
 }
 
 export default polls;
