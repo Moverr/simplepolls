7976f02eab1b58cf4a623bdcefe4658d85a8e6e6
Muyinda ROgers <moverr@gmail.com>
2020-10-08 13:30:48 +0300

    :Debuggging

--- src/admin/polls.jsx
+++ src/admin/polls.jsx
@@ -6,19 +6,44 @@ import Candidate from './Candidate';
 
 class polls extends Component {
 
-    componentDidMount(){
-        alert("Intersting");
+    constructor(props) {
+        super(props);
+        this.state = {
+			polls: null			 
+        };
+        
     }
+
+    componentDidMount(){
+         //fetch from server 
+         fetch('https://api.newvisionapp.com/v1/ElectoralCandidates?limit=6')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        });
+        
+
+
+    }
+    
     render() {
         return (
             <div className="wrapper">
-            <Candidate/>
+            {this.loadPolls()}
             <Candidate/>
             <Candidate/>
             <Candidate/>
             
             </div>
         );
+    }
+
+    loadPolls() {
+        let polls = Array();
+        polls.push(<Candidate />);
+        polls.push(<Candidate />);
+        polls.push(<Candidate />);
+        return  polls;
     }
 }
 
